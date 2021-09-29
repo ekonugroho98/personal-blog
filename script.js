65b62159c91c17cf78238c2a3a52f33cfa1dfe15
macbook <>
2021-09-29 09:34:43 +0700

    init commit

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
   })(jQuery);
 
   // input text for typing animation 
-  $("#holder").writeText("STUDENT + NOOBIE DEVELOPLER");
+  $("#holder").writeText("FULLSTACK DEVELOPER");
 
   // initialize wow.js
   new WOW().init();
